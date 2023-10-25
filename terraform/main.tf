99ae9f1e6b8d89b1d7817de2d5ea90f5e597e7a9
Grant Stoltman <gstoltman@gmail.com>
2023-10-24 20:30:21 -0400

    updating tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,56 @@ provider "google" {
   // credentials = file(var.credentials)  # Use this if you do not want to set env-var GOOGLE_APPLICATION_CREDENTIALS
 }
 
+resource "google_compute_instance" "airflow_vm_instance" {
+  name                      = "ldp-airflow-instance"
+  machine_type              = "e2-standard-4"
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = var.vm_image
+      size  = 30
+    }
+  }
+
+  network_interface {
+    network = var.network
+    access_config {
+    }
+  }
+}
+
+resource "google_storage_bucket" "bucket" {
+  name          = var.bucket
+  location      = var.region
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30 # days
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "stg_dataset" {
+  dataset_id                 = var.stg_bq_dataset
+  project                    = var.project
+  location                   = var.region
+  delete_contents_on_destroy = true
+}
+
+resource "google_bigquery_dataset" "prod_dataset" {
+  dataset_id                 = var.prod_bq_dataset
+  project                    = var.project
+  location                   = var.region
+  delete_contents_on_destroy = true
+}
+
 // Kafka config
 
 // Airflow config
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,9 @@
+locals {
+  data_lake_bucket = "ldp-data-lake"
+}
+
 variable "project" {
-  description = "linux-distro-popularity "
+  description = "Your GCP Project ID"
   type        = string
 }
 
@@ -12,5 +16,45 @@ variable "region" {
 variable "zone" {
   description = "Your project zone"
   default     = "us-west1-a"
+  type        = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "ldp-bq"
+}
+
+variable "vm_image" {
+  description = "Image for your VM"
+  default     = "ubuntu-os-cloud/ubuntu-2004-lts"
+  type        = string
+}
+
+variable "network" {
+  description = "Network for your instance/cluster"
+  default     = "default"
+  type        = string
+}
+
+variable "stg_bq_dataset" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default     = "ldp_stg"
+  type        = string
+}
+
+variable "prod_bq_dataset" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default     = "ldp_prod"
+  type        = string
+}
+
+variable "bucket" {
+  description = "The name of your bucket. This should be unique across GCP"
   type        = string
 }
